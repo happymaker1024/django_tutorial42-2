6253390e0eba849e57866a64dfded6e23ed66b0e
happymaker <happymaker1024@gmail.com>
2025-03-19 16:35:44 +0900

    index 페이지 기본 완성

--- django_tutor/urls.py
+++ django_tutor/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from community.views import articleList, viewDetail, write
+from community.views import articleList, index, viewDetail, write
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +9,7 @@ urlpatterns = [
     path('list/', articleList, name='list'),
     # http://127.0.0.1:8000/view_detail/1
     path('view_detail/<int:num>/', viewDetail, name='detail'),
+    # http://127.0.0.1:8000/
+    path('', index, name='index'),
     
 ]
