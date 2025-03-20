f8f6c96e083cb055c46f50345635d404544b7bb2
happymaker <happymaker1024@gmail.com>
2025-03-20 17:23:21 +0900

    템플릿 상속 구현, 콘텐츠에 부트스트랩 적용

--- django_tutor/settings.py
+++ django_tutor/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'community',
-    # 'crispy_forms',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
